56d82c15962899f95bdb0c91d107c9f34efc66b6
Nahom Assefa <nzenebe@gmail.com>
2021-11-06 13:24:53 -0500

    stored values in objects array

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+var isPlaying = true;
+var highScore = 0;
+
+
+const questionsArray = [{
+    question: "Which country does not border France",
+    options: ['Belgium', 'Switzerland', 'Luxembourg', 'Netherlands'],
+    correctAnswer: 'Netherlands',
+},
+{
+    question: "Which is not a province of Canada",
+    options: ['Yukon', 'Tromelin', 'Nova Scotia', 'New Brunswick'],
+    correctAnswer: 'Tromelin',
+},
+{
+    question: "Which modern state is not within the boundaries of the Louisiana purchase",
+    options: ['Idaho', 'North Dakota', 'Oklahoma', 'Nebraska'],
+    correctAnswer: 'Idaho',
+},
+{
+    question: "Which country did the famous Roman General and Statesman Pompey die",
+    options: ['Gaul', 'Greece', 'Egypt', 'Persia'],
+    correctAnswer: 'Egypt',
+},
+{
+    question: "Where is Julian Assange currently residing",
+    options: ['Russia', 'United States', 'Ecuador', 'England'],
+    correctAnswer: 'England',
+},
+{
+    question: "Which country did coffee originate in",
+    options: ['Brazil', 'Vietnam', 'Ethiopia', 'Philippines'],
+    correctAnswer: 'Ethiopia',
+},
+{
+    question: "Which country was Elon Musk born in",
+    options: ['England', 'Kenya', 'South Africa', 'Australia'],
+    correctAnswer: 'South Africa',
+},
+{
+    question: "Which country has Seoul as the capital",
+    options: ['South Korea', 'Indonesia', 'Myanmar', 'Pakistan'],
+    correctAnswer: 'South Korea',
+}]
+
